14e00b8099f2450b6a3885a8d1dbc477fe61b572
Juho Kokko <juho.kokko@sito.fi>
2016-11-17 18:37:21 +0200

    Added pymongo test function.

--- server/server.py
+++ server/server.py
@@ -1,16 +1,27 @@
 import tornado.ioloop
 import tornado.web
+from tornado.web import RequestHandler
+from pymongo import MongoClient
 
-class MainHandler(tornado.web.RequestHandler):
+
+class MainHandler(RequestHandler):
     def get(self):
         self.write("Backend is running!")
+
+
+class DbTestHandler(RequestHandler):
+    def get(self):
+        self.write(client.server_info())
+
 
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
+        (r"/db/", DbTestHandler),
     ])
 
 if __name__ == "__main__":
     app = make_app()
+    client = MongoClient("mongodb://localhost:27017")
     app.listen(80)
     tornado.ioloop.IOLoop.current().start()
