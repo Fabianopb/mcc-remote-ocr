83bff7bf40f83cee8ace863a4dbf68b6f2326756
Juho Kokko <jmekokko@gmail.com>
2016-11-19 20:43:34 +0200

    Implemented data storage in DB for initial OCR processing.

--- server/server.py
+++ server/server.py
@@ -6,6 +6,12 @@ import hashlib
 from PIL import Image
 import pytesseract
 from io import BytesIO
+from gridfs import GridFS
+from datetime import datetime
+
+
+THUMBNAIL_SIZE = 128, 128
+SOURCE_IMAGE_LIFETIME = 7
 
 
 class MainHandler(RequestHandler):
@@ -26,7 +32,7 @@ class AddUserHandler(RequestHandler):
         password = self.get_body_argument('password')
         hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
         user = {
-            'user_name': user,
+            'username': user,
             'password': hashed_password,
             'records': []
         }
@@ -41,11 +47,13 @@ class OCRTestHandler(RequestHandler):
         self.write(pytesseract.image_to_string(Image.open('test.jpg')))
 
 
+# Initial handler for OCR image processing
 class OCRHandler(RequestHandler):
-    # TODO: Change to self.current_user after authentication is done
-    user = 'test'
 
     def post(self):
+        # TODO: Change to self.current_user after authentication is done
+        username = 'test'
+
         # Check that 'images' form element is in the request
         try:
             image_files = self.request.files['images']
@@ -54,19 +62,59 @@ class OCRHandler(RequestHandler):
             self.finish('Missing \"images\" argument')
             return
 
+        image_count = len(image_files)
         # Check that at least one image was uploaded
-        if len(image_files) == 0:
+        if image_count == 0:
             self.set_status(400)
             self.finish('No images uploaded')
             return
 
         # TODO: Change code to do async processing
+        print('Received ' + str(image_count) + ' images')
         ocr_data = []
+        image_fs_id_array = []
+        fs = GridFS(db)
+
         for image in image_files:
-            print(image['filename'])
-            ocr_data.append(pytesseract.image_to_string(Image.open(BytesIO(image['body']))))
-        result = ' '.join(ocr_data)
-        self.write(result)
+            print('Processing ' + image['filename'])
+            pil_image = Image.open(BytesIO(image['body']))
+            ocr_data.append(pytesseract.image_to_string(pil_image))
+            thumbnail = create_thumbnail(pil_image)
+            image_fs_id = (fs.put(image['body'], content_type=image['content_type'], filename=image['filename']))
+            thumbnail_fs_id = (fs.put(thumbnail, content_type='image/jpeg', filename='t_' + image['filename']))
+            image_fs_id_array.append({'image_fs_id': image_fs_id, 'thumbnail_fs_id': thumbnail_fs_id})
+
+        ocr_result = '\n'.join(ocr_data)
+
+        ocr_record = {
+            'creation_time': datetime.utcnow(),
+            'image_fs_id_array': image_fs_id_array,
+            'ocr_text': ocr_result
+        }
+
+        print(ocr_record)
+
+        # TODO: Error handling and cleanup if database update fails
+        db.users.update_one({"username": username}, {'$push': {'records': ocr_record}})
+
+        self.write(ocr_result)
+
+
+def create_thumbnail(image):
+    """Create a thumbnail of the image given as a parameter, and returns it as a byte array in JPEG format
+
+    Keyword arguments:
+    image -- The image as a PIL Image object
+    """
+    image.thumbnail(THUMBNAIL_SIZE)
+    thumbnail_bytes = BytesIO()
+    image.save(thumbnail_bytes, format='JPEG')
+    return thumbnail_bytes.getvalue()
+
+# Get image from db
+#    filetest = fs.find_one({'filename': 'test.jpg'})
+#    self.set_header("Content-type", filetest.content_type)
+#    self.write(filetest.read())
 
 
 # URL routes etc.
@@ -79,6 +127,7 @@ def make_app():
         (r'/ocr/', OCRHandler),
     ])
 
+# Main app
 if __name__ == '__main__':
     app = make_app()
 
