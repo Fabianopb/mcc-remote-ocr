fe87ddbc7430a1cb8d08b4d58db785707c1dcfbc
Fabiano Brito <fabianopbrito@gmail.com>
2016-11-24 23:23:44 +0200

    Rock and rolls the https connection through self-signed certificate.

--- SelfCertifiedApp/app/src/main/java/com/temerarious/selfcertifiedapp/selfcertifiedapp/MainActivity.java
+++ SelfCertifiedApp/app/src/main/java/com/temerarious/selfcertifiedapp/selfcertifiedapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.temerarious.selfcertifiedapp.selfcertifiedapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnLogin = (Button) findViewById(R.id.testHttps);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ServerConnector serverConnector = new ServerConnector(MainActivity.this, MainActivity.this);
+                serverConnector.execute();
+            }
+        });
     }
+
 }
--- SelfCertifiedApp/app/src/main/java/com/temerarious/selfcertifiedapp/selfcertifiedapp/ServerConnector.java
+++ SelfCertifiedApp/app/src/main/java/com/temerarious/selfcertifiedapp/selfcertifiedapp/ServerConnector.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.widget.Toast;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,6 +13,20 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManagerFactory;
 
 /**
  * Created by fabiano.brito on 24/11/2016.
@@ -33,9 +48,56 @@ public class ServerConnector extends AsyncTask<String,Void,String> {
         String login_url = "https://130.211.96.63/";
 
         try {
+
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+
+            InputStream caInput = new BufferedInputStream(context.getAssets().open("nopass_cert.crt"));
+            Certificate ca = cf.generateCertificate(caInput);
+            System.out.println("ca=" + ((X509Certificate) ca).getSubjectDN());
+
+            String keyStoreType = KeyStore.getDefaultType();
+            KeyStore keyStore = KeyStore.getInstance(keyStoreType);
+            keyStore.load(null, null);
+            keyStore.setCertificateEntry("ca", ca);
+
+            String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
+            tmf.init(keyStore);
+
+            SSLContext context = SSLContext.getInstance("TLS");
+            context.init(null, tmf.getTrustManagers(), null);
+
             URL url = new URL(login_url);
+            HttpsURLConnection httpsURLConnection = (HttpsURLConnection)url.openConnection();
+            httpsURLConnection.setHostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            });
+            httpsURLConnection.setSSLSocketFactory(context.getSocketFactory());
+            httpsURLConnection.setRequestMethod("GET");
+            httpsURLConnection.setDoInput(true);
+            httpsURLConnection.setUseCaches(false);
+            httpsURLConnection.connect();
+
+            InputStream inputStream = httpsURLConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+            String result = "";
+            String line = "";
+            while ((line = bufferedReader.readLine()) != null) {
+                result += line;
+            }
+            bufferedReader.close();
+            inputStream.close();
+
+            httpsURLConnection.disconnect();
+            return result;
+
+            //URL url = new URL(login_url);
 
             // creating an http connection to communicate with url
+            /*
             HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
             httpURLConnection.setRequestMethod("GET");
             httpURLConnection.setDoInput(true);
@@ -55,10 +117,19 @@ public class ServerConnector extends AsyncTask<String,Void,String> {
 
             httpURLConnection.disconnect();
             return result;
+            */
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
             e.printStackTrace();
         }
         return null;
@@ -72,9 +143,9 @@ public class ServerConnector extends AsyncTask<String,Void,String> {
     @Override
     protected void onPostExecute(String result) {
         if(result != null) {
-            Toast.makeText(context, result, Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, result, Toast.LENGTH_LONG).show();
         } else {
-            Toast.makeText(context, "Nope...", Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, "Nope...", Toast.LENGTH_LONG).show();
         }
         loading.dismiss();
 
