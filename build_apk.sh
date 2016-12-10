9b7334359931915441c87a39fc77668bd8d5df83
Fabiano Brito <fabianopbrito@gmail.com>
2016-12-10 20:24:42 +0200

    Builds the debug app instead of the unsigned.

--- build_apk.sh
+++ build_apk.sh
@@ -4,6 +4,6 @@ echo 'Starting to build the APK...'
 
 cd TOCR-mobileUI/
 
-./gradlew assembleRelease
+./gradlew assembleDebug
 
 echo 'APK saved in TOCR-mobileUI/app/build/outputs/apk/app-release-unsigned.apk'
