d7158f4f4b69794535cf84fa0b12f43b08b922fc
Juho Kokko <jmekokko@gmail.com>
2016-12-11 21:45:34 +0200

    Added error check to build_apk.sh.

--- build_apk.sh
+++ build_apk.sh
@@ -6,4 +6,10 @@ cd TOCR-mobileUI/
 
 ./gradlew assembleRelease
 
-echo 'APK saved in TOCR-mobileUI/app/build/outputs/apk/app-release.apk'
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+    echo 'APK saved in TOCR-mobileUI/app/build/outputs/apk/app-release.apk.'
+else
+    (>&2 echo 'ERROR: APK build failed.')
+fi
+
