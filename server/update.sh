6999ee7ad7074b7e6b5ee0c5dff3fc23b002a56b
Juho Kokko <jmekokko@gmail.com>
2016-12-11 16:54:43 +0200

    Fixed update script.

--- server/update.sh
+++ server/update.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 docker build -t eu.gcr.io/mcc-2016-g13-p2/backend .
-gcloud docker -- push eu.gcr.io/mcc-2016-g13-p2/backend
-kubectl set image deployment/backend backend=eu.gcr.io/mcc-2016-g13-p2/backend
+gcloud docker -- push eu.gcr.io/mcc-2016-g13-p2/backend:latest
+kubectl set image deployment/backend backend=eu.gcr.io/mcc-2016-g13-p2/backend:latest
 echo 'Cluster updated.'
